839a65a19fa231a0d69f44fd77da6e0ce9d61b4f
JJudge0 <73240114+JJudge0@users.noreply.github.com>
2024-03-20 21:39:30 +0000

    testing file

--- test/test.cjs
+++ test/test.cjs
@@ -9,31 +9,22 @@ import('chai').then(chai => {
 const hardhat = require('hardhat');
 const ethers = hardhat.ethers;
 
+
 describe("StakingContract", function () {
-  let StakingContract;
+  let StakingExcercise;
   let stakingContract;
   let owner;
   let userA;
-
-  const REWARD_PER_DAY_PER_1000_DEFI = ethers.utils.parseEther("1");
-  const BLOCKS_PER_DAY = 86400 / 6; // Ethereum block time is 6 seconds and '86400' is total amount of seconds in a day   
+  let stakingToken;
 
   beforeEach(async function () {
     [owner, userA] = await ethers.getSigners();
 
-    // Deploy StakingContract
-    StakingContract = await ethers.getContractFactory("StakingContract");
-    stakingContract = await StakingContract.deploy(owner.address);
+    // Deploy StakingExcercise
+    StakingExcercise = await ethers.getContractFactory("StakingContract");
+    stakingContract = await StakingExcercise.deploy(stakingToken.address);
 
     // Approve tokens to the staking contract
-    // Ensure that stakingToken is an instance of ERC20 token contract
-    // For example, you can check if it has the 'approve' function
-    const stakingToken = await ethers.getContractAt("IERC20", await stakingContract.stakingToken());
-    if (typeof stakingToken.approve !== 'function') {
-      throw new Error("stakingToken does not support 'approve' function");
-    }
-
-    // Now you can safely call the approve function
     await stakingToken.connect(userA).approve(stakingContract.address, ethers.constants.MaxUint256);
   });
 
@@ -57,7 +48,7 @@ describe("StakingContract", function () {
     await ethers.provider.send("evm_mine");
 
     const rewards = await stakingContract.calculateRewards(userA.address);
-    const expectedRewards = amount.mul(BLOCKS_PER_DAY * 10).div(1000);
+    const expectedRewards = amount.mul(10).div(1000); // 1 DEFI per day per 1000 DEFI staked
     expect(rewards).to.equal(expectedRewards, "Incorrect rewards calculated");
   });
 
@@ -69,15 +60,25 @@ describe("StakingContract", function () {
     await ethers.provider.send("evm_increaseTime", [10 * 24 * 60 * 60]);
     await ethers.provider.send("evm_mine");
 
-    const initialBalance = await ethers.provider.getBalance(userA.address);
     await stakingContract.connect(userA).withdraw();
 
-    const finalBalance = await ethers.provider.getBalance(userA.address);
-    const rewards = finalBalance.sub(initialBalance);
+    const stakedBalance = await stakingContract.stakedBalance(userA.address);
+    expect(stakedBalance).to.equal(0, "Staked balance not reset after withdrawal");
 
-    const expectedRewards = amount.mul(BLOCKS_PER_DAY * 10).div(1000);
-    expect(rewards).to.equal(expectedRewards.add(amount), "Incorrect rewards and staked amount withdrawn");
+    const lastStakedTime = await stakingContract.lastStakedTime(userA.address);
+    expect(lastStakedTime).to.equal(0, "Last staked time not reset after withdrawal");
+  });
 
-    
+  it("should return correct rewards with viewRewards", async function () {
+    const amount = ethers.utils.parseEther("1000");
+    await stakingContract.connect(userA).stake(amount);
+
+    // Fast-forward 10 days
+    await ethers.provider.send("evm_increaseTime", [10 * 24 * 60 * 60]);
+    await ethers.provider.send("evm_mine");
+
+    const rewards = await stakingContract.viewRewards(userA.address);
+    const expectedRewards = amount.mul(10).div(1000); // 1 DEFI per day per 1000 DEFI staked
+    expect(rewards).to.equal(expectedRewards, "Incorrect rewards returned by viewRewards");
   });
 });
